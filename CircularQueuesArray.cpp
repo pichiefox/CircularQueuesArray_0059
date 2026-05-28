dfd3e43561f4ab67e28bcf3cef5e4fc8645cf5db
pichiefox <citrazad@gmail.com>
2026-05-28 18:34:32 +0700

    jika rear berada di posisi terakhir array, kembali ke awal array

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -35,3 +35,12 @@ public:
         }
         else
         {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }  
+        queue_array[REAR] = num;
+    }
+
