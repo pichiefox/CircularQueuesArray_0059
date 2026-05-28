ed0fbcc99fec5a41c6fb8074db73194dfa6b9036
pichiefox <citrazad@gmail.com>
2026-05-28 18:40:04 +0700

    jika FRONT <= REAR, iterasi dari FRONT hingga akhir array

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -96,3 +96,11 @@ public:
         }
         else
         {
+            // jika FRONT <= REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            FRONT_Position = 0;
+
