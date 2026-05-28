1a342c769a864235d1ba7eeb668a5cf787b0e791
pichiefox <citrazad@gmail.com>
2026-05-28 18:37:18 +0700

    cek jika antrian hanya memiliki satu elemen

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -54,3 +54,11 @@ public:
         }
         cout << "\nThe element deleted from the queue is: " << queue_array [FRONT]<< "\n";
 
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
